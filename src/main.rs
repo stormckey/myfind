69d341d54368ead8a0f1fc1e71dd16363203a038
stormckey <sortygraph@gmail.com>
2023-09-04 23:47:55 +0800

    use tracing to log some simple info

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,15 @@ use std::process;
 use util::find;
 use regex::Regex;
 use cli::check_cli;
-use colored::*;
 use cli::Cli;
+use colored::*;
+use tracing::{span, Level, info};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 fn main() {
+    tracing_subscriber::registry().with(fmt::layer()).init();
+    let scope = span!(Level::DEBUG, "main");
+    let _enter = scope.enter();
     let mut cli = Cli::parse();
     check_cli(&mut cli);
     let regex = match Regex::new(cli.regex.join("|").as_str()){
@@ -18,12 +23,13 @@ fn main() {
             process::exit(1);
         }
     };
+    info!("The regex now is {}", regex.as_str());
     for path in cli.path{
 
         if cli.verbose{
             println!("Now search in path : {}",path);
         }
-
+        info!("The path to be searched now is {}",path);
         match find(path,&regex) {
             Ok(mut matches) => {
                 if matches.is_empty(){
@@ -50,5 +56,3 @@ fn main() {
         }
     }
 }
-
-
