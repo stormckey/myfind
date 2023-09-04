59a57d0f0a91474ac71c478a0f61c0b3bf5cbd6d
stormckey <sortygraph@gmail.com>
2023-09-04 16:51:17 +0800

    implement sorted outputing

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ My customized version of the `find` command implemented in Rust.
 ## Usage
 
 ```
-./target/debug/myfind -p path1 -p path2 -r regex1 -r regex2 [-v|-asc|-desc|-h]
+./target/debug/myfind -p path1 -p path2 -r regex1 -r regex2 [-v|--asc|--desc|-h]
 ```
+
+differenet regexes will be connected by `OR` operator.
 
 use `-h` to get help.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod cli;
 mod util;
-use tracing::{span,Level};
+// use tracing::{span,Level};
 use clap::Parser;
 use std::process;
 use util::find;
@@ -10,8 +10,8 @@ use colored::*;
 use cli::Cli;
 
 fn main() {
-    let span = span!(Level::TRACE,"my span");
-    let span = span.enter();
+    // let span = span!(Level::TRACE,"my span");
+    // let span = span.enter();
     let mut cli = Cli::parse();
     check_cli(&mut cli);
     let regex = get_regex(cli.regex);
@@ -22,11 +22,16 @@ fn main() {
         }
 
         match find(path,&regex) {
-            Ok(matches) => {
+            Ok(mut matches) => {
                 if matches.is_empty(){
                     println!("No match.");
                 } else {
                     println!("find the following mathces: ");
+                    if cli.asc{
+                        matches.sort();
+                    }else if cli.desc{
+                        matches.sort_by(|a, b| b.cmp(a));
+                    }
                     for path in matches {
                         if let Some(index) = path.rfind('/'){
                             let (p,f) = path.split_at(index+1);
